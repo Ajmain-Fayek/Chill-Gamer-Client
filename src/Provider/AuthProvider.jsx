3151caf7073356f80da86b04464f2692321db477
Ajmain Fayek <ajmainfayek733@gmail.com>
2024-12-06 00:43:30 +0600

    firebase dir removed from github

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,11 +1,81 @@
-import { createContext, useState } from "react";
-
+import { createContext, useState, useEffect } from "react";
+import {
+    createUserWithEmailAndPassword,
+    deleteUser,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+} from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 export const AuthContext = createContext();
+// GOOGLE AUTH PROVDER
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
     // Theme Toggler
     const [themeToggle, setThemeToggle] = useState(true);
-    const authInfo = { themeToggle, setThemeToggle };
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Sign Up with **EMAIL & PASSWORD**
+    const signUpWithEmailAndPassword = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    // Sign in with **EMAIL & PASSWORD**
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // Sign in with **GOOGLE**
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    // Update USER INFO
+    const updateUserInfo = (obj) => {
+        setLoading(true);
+        return updateProfile(auth.currentUser, obj);
+    };
+
+    // Sign Out User
+    const logOutUser = () => {
+        return signOut(auth);
+    };
+
+    // DELETE User Account
+    const deleteAccount = () => {
+        return deleteUser(auth.currentUser);
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            fetch(`http://localhost:8800/users/${currentUser.email}`)
+                .then((res) => res.json())
+                .then((usr) => setUser(usr))
+                .then(setLoading(false));
+        });
+
+        return () => unsubscribe();
+    });
+    const authInfo = {
+        themeToggle,
+        setThemeToggle,
+        signUpWithEmailAndPassword,
+        signInWithGoogle,
+        updateUserInfo,
+        deleteAccount,
+        setUser,
+        logOutUser,
+        signInUser,
+        user,
+        loading,
+    };
     return (
         <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
     );
--- src/Provider/PrivateRouter.jsx
+++ src/Provider/PrivateRouter.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Login } from "../Components/Login";
+import React, { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
 
 const PrivateRouter = ({ children }) => {
-    // if (loading) return <h1>loading</h1>;
-    // if (user) return children;
-    return children;
+    const { user, loading } = useContext(AuthContext);
+
+    // If loading true
+    if (loading) {
+        return (
+            <span className="block mx-auto mt-10 loading loading-spinner text-success"></span>
+        );
+    }
+
+    //  If user found
+    if (user) return children;
+
+    //  If no User found
+    return <Navigate to="/login" />;
 };
 
 export default PrivateRouter;
